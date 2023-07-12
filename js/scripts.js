a2a28182a239a62bdbcabdb07056fefb11cafa62
danielveg4 <dvgilles@gmail.com>
2023-07-12 16:54:53 +0200

    comit

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,8 @@
 const firstButton = document.getElementById('firstButton');
 
 firstButton.addEventListener('click', event => {
-    firstButton.removeChild(event.target);
+  if (event.target.tagName !== 'BUTTON') return;
+  event.target.remove();
 });
 
 // Crea una función que elimine el botón siguiente al que has pulsado, si no hubiera botón siguiente, elimina el anterior, y no hay ni anterior ni siguiente se elimina el mismo.
@@ -13,18 +14,21 @@ firstButton.addEventListener('click', event => {
 const secondButton = document.getElementById('secondButton');
 
 secondButton.addEventListener('click', event => {
-    const nextButton = event.target.nextElementSibling;
-    const prevButton = event.target.previousElementSibling;
-    if (nextButton) {
-        secondButton.removeChild(nextButton);
-        console.log('Eliminado botón siguiente');
-    } else if (prevButton) {
-        secondButton.removeChild(event.target.previousElementSibling);
-        console.log('Eliminado botón anterior');
-    } else {
-        secondButton.removeChild(event.target);
-        console.log('Eliminado el botón pulsado');
-    }
+  if (event.target.tagName !== 'BUTTON') return;
+  const nextButton = event.target.nextElementSibling;
+  const prevButton = event.target.previousElementSibling;
+  if (nextButton) {
+    nextButton.remove();
+    console.log('Eliminado botón siguiente');
+  } else if (prevButton) {
+    prevButton.remove();
+
+    console.log('Eliminado botón anterior');
+  } else {
+    event.target.remove();
+
+    console.log('Eliminado el botón pulsado');
+  }
 });
 
 //Crea una función que permita añadir elementos a la lista con la misma estructura que te doy en el ejemplo, además cada vez que pulses el botón de la X debe eliminar el elemento de la lista que has pulsado.
@@ -32,17 +36,21 @@ secondButton.addEventListener('click', event => {
 const thirtButton = document.getElementById('thirtButton');
 const list = document.getElementById('list');
 
-thirtButton.addEventListener('click', () => {
+thirtButton.addEventListener('click', event => {
+  if (event.target.tagName !== 'BUTTON') return;
   const newItem = document.createElement('li');
-  const itemText = document.createTextNode(`Elemento ${list.childElementCount + 1}`);
+  const itemText = document.createTextNode(
+    `Elemento ${list.childElementCount + 1}`
+  );
   const deleteButton = document.createElement('button');
   deleteButton.textContent = ' X ';
-
-  deleteButton.addEventListener('click', () => {
-    list.removeChild(newItem);
-  });
 
   newItem.appendChild(itemText);
   newItem.appendChild(deleteButton);
   list.appendChild(newItem);
+});
+
+list.addEventListener('click', event => {
+  if (event.target.tagName !== 'BUTTON') return;
+  event.target.parentElement.remove();
 });
